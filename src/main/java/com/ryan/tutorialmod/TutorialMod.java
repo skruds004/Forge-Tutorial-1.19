3d2d3f11d13fa693e1b5607c8aa58a57fd01af2a
Ryan Hasler <rhasler004@gmail.com>
2022-12-08 01:42:44 -0500

    Added zircon and raw_zircon items and a creative mode tab

--- src/main/java/com/ryan/tutorialmod/TutorialMod.java
+++ src/main/java/com/ryan/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package com.ryan.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import com.ryan.tutorialmod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,6 +22,9 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
     }
