7638779eea7400ec48f6233548baaac494058667
Ryan Hasler <rhasler004@gmail.com>
2022-12-11 02:30:39 -0500

    Added a Magic Eight Ball that gives you a number. What could it mean?

--- src/main/java/com/ryan/tutorialmod/item/ModItems.java
+++ src/main/java/com/ryan/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.ryan.tutorialmod.item;
 
 import com.ryan.tutorialmod.TutorialMod;
+import com.ryan.tutorialmod.item.custom.EightBallItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,6 +19,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
